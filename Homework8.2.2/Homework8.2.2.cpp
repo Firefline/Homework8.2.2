0be65ac2f147f330b6ca172b7fb77a4731061674
Vladimir Romanov <valgard5@mail.ru>
2023-04-25 20:12:24 +0300

    initial commit

---
+++ Homework8.2.2/Homework8.2.2.cpp
@@ -0,0 +1,21 @@
+﻿// Homework8.2.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include "proxy.cpp"
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(std::addressof(real_db), 2);
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+
+    return 0;
+}
